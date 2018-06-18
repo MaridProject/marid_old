60cdb9b5ac41b311848f22166f177541d77970e8
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2018-06-18 07:11:37 +0300

    EPL

--- marid-webapp/src/main/java/org/marid/applib/dialogs/ShellDialog.java
+++ marid-webapp/src/main/java/org/marid/applib/dialogs/ShellDialog.java
@@ -13,18 +13,53 @@
  */
 package org.marid.applib.dialogs;
 
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.intellij.lang.annotations.MagicConstant;
+import org.marid.applib.image.AppImage;
+import org.marid.applib.image.WithImages;
 
-public class ShellDialog extends Shell {
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
-  public ShellDialog(Shell parent,
-                     @MagicConstant(flagsFromClass = SWT.class) int style,
-                     @MagicConstant(valuesFromClass = IDialogConstants.class) int... buttons) {
+public class ShellDialog extends Shell implements WithImages {
+
+  public ShellDialog(Shell parent, @MagicConstant(flagsFromClass = SWT.class) int style) {
     super(parent, style);
     setLayout(new GridLayout(1, false));
+  }
+
+  @SafeVarargs
+  public final void addButton(String text, AppImage image, Listener listener, Consumer<Button>... buttonConsumers) {
+    final var buttons = Stream.of(getChildren())
+        .filter(Composite.class::isInstance)
+        .map(Composite.class::cast)
+        .filter(c -> "buttons".equals(c.getData("dialogControlType")))
+        .findFirst()
+        .orElseGet(() -> {
+          final var c = new Composite(this, SWT.NONE);
+          final var l = new GridLayout(1, false);
+
+          l.marginWidth = 10;
+          l.marginHeight = 10;
+          l.horizontalSpacing = 10;
+
+          c.setLayout(l);
+          c.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+          c.setData("dialogControlType", "buttons");
+          return c;
+        });
+    final var button = new Button(buttons, SWT.PUSH);
+    button.setText(text);
+    button.setImage(image(image));
+    button.addListener(SWT.Selection, listener);
+    for (final var buttonConsumer : buttonConsumers) {
+      buttonConsumer.accept(button);
+    }
   }
 }
