78a88e2092c60a531bce9db5f969ced497ea4c0b
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2019-07-12 22:48:47 +0300

    Revert: Upgrade plugins/dependencies

--- marid-ide/marid-ide-server/src/main/java/org/marid/app/web/rap/RapAppConfig.java
+++ marid-ide/marid-ide-server/src/main/java/org/marid/app/web/rap/RapAppConfig.java
@@ -3,9 +3,9 @@ package org.marid.app.web.rap;
 import org.eclipse.rap.rwt.application.Application;
 import org.eclipse.rap.rwt.application.ApplicationConfiguration;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.springframework.stereotype.Component;
@@ -24,8 +24,11 @@ public class RapAppConfig implements ApplicationConfiguration {
       shell.setMaximized(true);
       shell.setLayout(new GridLayout(1, false));
 
-      final var composite = new ScrolledComposite(shell, SWT.H_SCROLL | SWT.V_SCROLL);
+      final var composite = new Composite(shell, SWT.H_SCROLL | SWT.V_SCROLL);
+      composite.setLayout(new GridLayout(1, false));
+
       final var button = new Button(composite, SWT.NONE);
+      button.setText("BUTTON");
 
       shell.layout();
       shell.open();
