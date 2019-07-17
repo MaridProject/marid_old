33c97c772a4fc9d66cc5dfa827ed1aaed3048592
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2019-07-17 21:25:22 +0300

    ExitCommand

--- marid-ide/marid-ide-client/src/main/java/org/marid/ide/images/ImageCache.java
+++ marid-ide/marid-ide-client/src/main/java/org/marid/ide/images/ImageCache.java
@@ -1,6 +1,7 @@
 package org.marid.ide.images;
 
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.widgets.Display;
 import org.intellij.lang.annotations.Language;
 import org.marid.image.MaridIcon;
@@ -19,24 +20,24 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ImageCache {
 
   private final Display mainDisplay;
-  private final ConcurrentHashMap<Integer, Image> images = new ConcurrentHashMap<>();
+  private final ConcurrentHashMap<String, ImageData> images = new ConcurrentHashMap<>();
 
   public ImageCache(Display mainDisplay) {
     this.mainDisplay = mainDisplay;
   }
 
-  public Image image(BufferedImage image) {
+  private ImageData imageData(BufferedImage image) {
     final var bos = new ByteArrayOutputStream(8192);
     try {
       ImageIO.write(image, "PNG", bos);
     } catch (IOException e) {
       throw new UncheckedIOException(e);
     }
-    return new Image(mainDisplay, new ByteArrayInputStream(bos.toByteArray()));
+    return new ImageData(new ByteArrayInputStream(bos.toByteArray()));
   }
 
   public Image icon(int size, Color color) {
-    return images.computeIfAbsent(size, s -> image(MaridIcon.getImage(s, color)));
+    return new Image(mainDisplay, images.computeIfAbsent("icon" + size, s -> imageData(MaridIcon.getImage(size, color))));
   }
 
   public Image icon(int size) {
@@ -44,10 +45,12 @@ public class ImageCache {
   }
 
   public Image image(@Language(value = "groovy", prefix = "Thread.currentThread().getContextClassLoader().getResourceAsStream(\"images/", suffix = "\")") String resourcePath) {
-    try (final var is = Thread.currentThread().getContextClassLoader().getResourceAsStream(resourcePath)) {
-      return new Image(mainDisplay, is);
-    } catch (IOException e) {
-      throw new UncheckedIOException(e);
-    }
+    return new Image(mainDisplay, images.computeIfAbsent(resourcePath, p -> {
+      try (final var is = Thread.currentThread().getContextClassLoader().getResourceAsStream(p)) {
+        return new ImageData(is);
+      } catch (IOException e) {
+        throw new UncheckedIOException(e);
+      }
+    }));
   }
 }
