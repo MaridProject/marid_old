f560cddbe82e9b699621ffaff5319339f8f3637b
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2019-07-19 21:14:24 +0300

    ExitCommand

--- marid-ide/marid-ide-client/src/main/java/org/marid/ide/library/LibraryViewer.java
+++ marid-ide/marid-ide-client/src/main/java/org/marid/ide/library/LibraryViewer.java
@@ -1,16 +1,21 @@
 package org.marid.ide.library;
 
-import org.eclipse.jface.viewers.ListViewer;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Tree;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
 @Component
 @DependsOn("librarySelector")
-public class LibraryViewer extends ListViewer {
+public class LibraryViewer extends Tree {
 
   public LibraryViewer(LibraryPane libraryPane) {
-    super(libraryPane);
-    getControl().setLayoutData(new GridData(GridData.FILL_BOTH));
+    super(libraryPane, SWT.SINGLE | SWT.FULL_SELECTION | SWT.BORDER);
+    setLayoutData(new GridData(GridData.FILL_BOTH));
+  }
+
+  @Override
+  protected void checkSubclass() {
   }
 }
