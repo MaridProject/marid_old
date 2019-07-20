31e3c95fc93944b68fcef16f9cf22574acfd456c
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2019-07-20 13:26:53 +0300

    ExitCommand

--- marid-types/src/test/java/org/marid/ecj/EcjTest.java
+++ marid-types/src/test/java/org/marid/ecj/EcjTest.java
@@ -21,19 +21,34 @@ package org.marid.ecj;
  * #L%
  */
 
+import org.eclipse.jdt.core.compiler.CompilationProgress;
+import org.eclipse.jdt.internal.compiler.CompilationResult;
+import org.eclipse.jdt.internal.compiler.Compiler;
+import org.eclipse.jdt.internal.compiler.IErrorHandlingPolicy;
+import org.eclipse.jdt.internal.compiler.IProblemFactory;
 import org.eclipse.jdt.internal.compiler.batch.FileSystem;
+import org.eclipse.jdt.internal.compiler.classfmt.ClassFileConstants;
+import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
+import org.eclipse.jdt.internal.compiler.problem.DefaultProblemFactory;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.marid.test.spring.TempFolder;
+import org.mockito.Mockito;
+import org.mockito.internal.creation.MockSettingsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.stream.Stream;
 
 @Tag("manual")
@@ -42,27 +57,93 @@ import java.util.stream.Stream;
 class EcjTest {
 
   @Autowired
-  private Path classPathFolder1;
+  private ArrayList<CompilationResult> results;
 
   @Test
   void testCompilation() {
-    final var fileSystem = new FileSystem(new String[0], new String[] {"Test"}, "UTF-8");
   }
 
   @Configuration
   public static class TestContext {
 
     @Bean
-    public TempFolder sourceFolder() {
+    public Path sourceFolder() throws Exception {
+      return Path.of(EcjTest.class.getResource("TestFile.java").toURI());
+    }
+
+    @Bean
+    public TempFolder targetFolder() {
       return new TempFolder("ecjtest");
     }
 
     @Bean
-    public String[] classPathNames(@Qualifier("classpath") Path[] folders) {
-      return Stream.of(folders)
-          .map(Path::toAbsolutePath)
-          .map(Path::toString)
-          .toArray(String[]::new);
+    public Path[] javaFiles(Path sourceFolder) throws Exception {
+      return Files.find(sourceFolder, 3, (p, a) -> p.getFileName().toString().endsWith(".java")).toArray(Path[]::new);
+    }
+
+    @Bean
+    public FileSystem fileSystem(Path[] javaFiles) {
+      return new FileSystem(new String[0], Stream.of(javaFiles).map(p -> p.getFileName().toString()).toArray(String[]::new), "UTF-8");
+    }
+
+    @Bean
+    public IErrorHandlingPolicy errorHandlingPolicy() {
+      final var handlingPolicy = Mockito.mock(IErrorHandlingPolicy.class, new MockSettingsImpl<>().stubOnly());
+      Mockito.when(handlingPolicy.stopOnFirstError()).thenReturn(true);
+      return handlingPolicy;
+    }
+
+    @Bean
+    public CompilerOptions compilerOptions() {
+      final var compilerOptions = new CompilerOptions();
+      compilerOptions.complianceLevel = compilerOptions.originalComplianceLevel = ClassFileConstants.JDK11;
+      compilerOptions.sourceLevel = compilerOptions.originalSourceLevel = ClassFileConstants.JDK11;
+      compilerOptions.targetJDK = ClassFileConstants.JDK11;
+      compilerOptions.produceReferenceInfo = true;
+      compilerOptions.preserveAllLocalVariables = true;
+      compilerOptions.produceMethodParameters = true;
+      compilerOptions.generateClassFiles = false;
+      return compilerOptions;
+    }
+
+    @Bean
+    public ArrayList<CompilationResult> compilationResults() {
+      return new ArrayList<>();
+    }
+
+    @Bean
+    public IProblemFactory problemFactory() {
+      return new DefaultProblemFactory(Locale.US);
+    }
+
+    @Bean
+    public StringWriter output() {
+      return new StringWriter();
+    }
+
+    @Bean
+    public Compiler compiler(FileSystem fileSystem,
+                             IErrorHandlingPolicy errorHandlingPolicy,
+                             CompilerOptions compilerOptions,
+                             ArrayList<CompilationResult> compilationResults,
+                             IProblemFactory problemFactory,
+                             StringWriter output) {
+
+      return new Compiler(
+          fileSystem,
+          errorHandlingPolicy,
+          compilerOptions,
+          compilationResults::add,
+          problemFactory,
+          new PrintWriter(output),
+          Mockito.mock(CompilationProgress.class, new MockSettingsImpl<>().stubOnly())
+      );
+    }
+
+    @Bean(initMethod = "run")
+    public Runnable compilationTask(Compiler compiler, Path targetFolder) throws IOException {
+      return () -> {
+      };
     }
   }
 }
---
+++ marid-types/src/test/resources/org/marid/ecj/TestFile.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class TestFile {
+
+  public void test1() {
+    var v = Arrays.asList("a", 1);
+  }
+
+}
