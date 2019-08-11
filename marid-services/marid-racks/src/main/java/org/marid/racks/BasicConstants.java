e166c0da9b467b88b5c2c2a4fdcc1abd9fee83a6
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2019-08-11 20:32:45 +0300

    Add some dependencies

---
+++ marid-services/marid-racks/src/main/java/org/marid/racks/BasicConstants.java
@@ -0,0 +1,38 @@
+package org.marid.racks;
+
+import org.marid.runtime.annotation.Constant;
+import org.marid.runtime.annotation.Constants;
+
+@Constants
+public interface BasicConstants {
+
+  @Constant
+  static int intConstant(int value) {
+    return value;
+  }
+
+  @Constant
+  static int intUnsigned(long value) {
+    return (int) (value & 0xFFFF_FFFFL);
+  }
+
+  @Constant
+  static long longConstant(long value) {
+    return value;
+  }
+
+  @Constant
+  static long longUnsigned(String value) {
+    return Long.parseUnsignedLong(value);
+  }
+
+  @Constant
+  static short shortConstant(short value) {
+    return value;
+  }
+
+  @Constant
+  static short shortUnsigned(int value) {
+    return (short) (value & 0xFFFF);
+  }
+}
---
+++ marid-services/marid-racks/src/main/java/org/marid/racks/net/NetConstants.java
@@ -0,0 +1,73 @@
+package org.marid.racks.net;
+
+/*-
+ * #%L
+ * marid-racks
+ * %%
+ * Copyright (C) 2012 - 2019 MARID software development group
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+import org.marid.runtime.annotation.Constant;
+import org.marid.runtime.annotation.Constants;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+
+@Constants
+public interface NetConstants {
+
+  @Constant
+  static InetAddress inetAddressFromBytes(byte[] address) throws UnknownHostException {
+    return InetAddress.getByAddress(address);
+  }
+
+  @Constant
+  static InetAddress inetAddressFromHostAndBytes(String host, byte[] address) throws UnknownHostException {
+    return InetAddress.getByAddress(host, address);
+  }
+
+  @Constant
+  static InetAddress inetAddressFromHost(String host) throws UnknownHostException {
+    return InetAddress.getByName(host);
+  }
+
+  @Constant
+  static InetAddress anyLocalAddress() {
+    return new InetSocketAddress(0).getAddress();
+  }
+
+  @Constant
+  static InetAddress loopbackAddress() {
+    return InetAddress.getLoopbackAddress();
+  }
+
+  @Constant
+  static InetSocketAddress socketAddress(String host, int port) {
+    return new InetSocketAddress(host, port);
+  }
+
+  @Constant
+  static InetSocketAddress socketAddressUnresolved(String host, int port) {
+    return InetSocketAddress.createUnresolved(host, port);
+  }
+
+  @Constant
+  static InetSocketAddress socketAddressFromInetAddress(InetAddress address, int port) {
+    return new InetSocketAddress(address, port);
+  }
+}
