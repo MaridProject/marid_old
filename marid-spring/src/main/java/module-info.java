5368f573f9a11ea6cee1f4ab6bc107748bbd376d
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2019-10-08 19:40:39 +0300

    Added some javadocs

---
+++ marid-spring/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module marid.spring {
+
+  requires static org.jetbrains.annotations;
+
+  requires spring.context;
+  requires spring.aop;
+  requires spring.beans;
+  requires spring.core;
+  requires spring.expression;
+  requires spring.jcl;
+}
