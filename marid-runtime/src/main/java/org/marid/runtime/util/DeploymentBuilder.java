4628025043eddc522ce3abc74dd7e5f0f971e15b
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2019-10-08 21:09:19 +0300

    Added some javadocs

--- marid-runtime/src/main/java/org/marid/runtime/util/DeploymentBuilder.java
+++ marid-runtime/src/main/java/org/marid/runtime/util/DeploymentBuilder.java
@@ -10,12 +10,12 @@ package org.marid.runtime.util;
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * #L%
@@ -254,7 +254,6 @@ public final class DeploymentBuilder {
   }
 
   private interface Input {
-
     InputStream inputStream() throws IOException;
   }
 }
