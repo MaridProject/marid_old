0ccccff609fbc35b5065e9600fb072fade16f5dc
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2019-11-19 21:38:28 +0300

    Upgrade dependencies

--- marid-runtime/src/test/java/org/marid/runtime/DynalinkTest.java
+++ marid-runtime/src/test/java/org/marid/runtime/DynalinkTest.java
@@ -22,37 +22,89 @@ package org.marid.runtime;
  */
 
 import jdk.dynalink.CallSiteDescriptor;
+import jdk.dynalink.DynamicLinker;
 import jdk.dynalink.DynamicLinkerFactory;
 import jdk.dynalink.beans.BeansLinker;
+import jdk.dynalink.beans.StaticClass;
 import jdk.dynalink.support.SimpleRelinkableCallSite;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.marid.runtime.test.impl.TestBean;
 
-import java.lang.invoke.MethodHandles;
-
+import static java.lang.invoke.MethodHandles.publicLookup;
 import static java.lang.invoke.MethodType.methodType;
+import static jdk.dynalink.StandardNamespace.METHOD;
 import static jdk.dynalink.StandardNamespace.PROPERTY;
+import static jdk.dynalink.StandardOperation.CALL;
 import static jdk.dynalink.StandardOperation.GET;
+import static jdk.dynalink.StandardOperation.NEW;
+import static jdk.dynalink.StandardOperation.SET;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Tag("normal")
 class DynalinkTest {
 
-  @Test
-  void test() throws Throwable {
+  private final DynamicLinker linker;
+
+  public DynalinkTest() {
     final var linkerFactory = new DynamicLinkerFactory();
     linkerFactory.setPrioritizedLinker(new BeansLinker());
+    linker = linkerFactory.createLinker();
+  }
 
-    final var linker = linkerFactory.createLinker();
-
-    final var lookup = MethodHandles.publicLookup();
+  @Test
+  void get() throws Throwable {
     final var op = GET.withNamespace(PROPERTY).named("x");
-    final var csd = new CallSiteDescriptor(lookup, op, methodType(Object.class, Object.class));
+    final var csd = new CallSiteDescriptor(publicLookup(), op, methodType(Object.class, Object.class));
 
     final var site = linker.link(new SimpleRelinkableCallSite(csd));
     final var mh = site.dynamicInvoker().bindTo(new TestBean());
 
     assertEquals("x", mh.invoke());
+  }
+
+  @Test
+  void constructor() throws Throwable {
+    final var op = NEW.named("new");
+    final var csd = new CallSiteDescriptor(publicLookup(), op, methodType(Object.class, StaticClass.class));
+
+    final var site = linker.link(new SimpleRelinkableCallSite(csd));
+    final var mh = site.dynamicInvoker();
+
+    assertTrue(mh.invoke(StaticClass.forClass(TestBean.class)) instanceof TestBean);
+  }
+
+  @Test
+  void setter() throws Throwable {
+    final var op = SET.withNamespace(PROPERTY).named("length");
+    final var csd = new CallSiteDescriptor(publicLookup(), op, methodType(void.class, Object.class, Object.class));
+
+    final var builder = new StringBuilder();
+    final var site = linker.link(new SimpleRelinkableCallSite(csd));
+    final var mh = site.dynamicInvoker().bindTo(builder);
+
+    mh.invoke(3);
+
+    assertEquals(3, builder.length());
+  }
+
+  @Test
+  void call() throws Throwable {
+    final var builder = new StringBuilder("abcde");
+
+    final var callable = linker.link(new SimpleRelinkableCallSite(new CallSiteDescriptor(
+        publicLookup(),
+        GET.withNamespace(METHOD).named("replace"),
+        methodType(Object.class, Object.class)
+    ))).dynamicInvoker().bindTo(builder).invoke();
+
+    linker.link(new SimpleRelinkableCallSite(new CallSiteDescriptor(
+        publicLookup(),
+        CALL,
+        methodType(Object.class, Object.class, Object.class, Object.class, Object.class, Object.class)
+    ))).dynamicInvoker().invoke(callable, builder, 0, 2, "");
+
+    assertEquals("cde", builder.toString());
   }
 }
