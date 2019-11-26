160902863308cebcbffe4f1e262eca89af23bd04
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2019-11-26 19:01:33 +0300

    Upgrade dependencies

--- marid-runtime/src/main/java/org/marid/runtime/internal/WineryRuntime.java
+++ marid-runtime/src/main/java/org/marid/runtime/internal/WineryRuntime.java
@@ -23,7 +23,7 @@ package org.marid.runtime.internal;
 
 import jdk.dynalink.DynamicLinker;
 import jdk.dynalink.DynamicLinkerFactory;
-import jdk.dynalink.beans.BeansLinker;
+import org.jetbrains.annotations.TestOnly;
 import org.marid.io.MaridFiles;
 import org.marid.io.Xmls;
 import org.marid.runtime.exception.WineryCloseException;
@@ -32,7 +32,6 @@ import org.marid.runtime.model.Winery;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.StreamCorruptedException;
-import java.lang.ref.Cleaner;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
@@ -50,17 +49,11 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 public final class WineryRuntime implements AutoCloseable {
 
-  private final Cleaner cleaner = Cleaner.create();
-  private final Path deployment;
-  private final Path deps;
-  private final Path resources;
-  private final Path classes;
-  private final Path winery;
   private final Thread thread;
   private final LinkedTransferQueue<Command> queue = new LinkedTransferQueue<>();
+  private final AutoCloseable destroyAction;
 
-  final DynamicLinker linker;
-  final List<String> args;
+  final DynamicLinker linker = new DynamicLinkerFactory().createLinker();
   final URLClassLoader classLoader;
   final LinkedHashMap<String, CellarRuntime> cellars = new LinkedHashMap<>();
 
@@ -68,70 +61,51 @@ public final class WineryRuntime implements AutoCloseable {
   private volatile Throwable startError;
   private volatile Throwable destroyError;
 
-  public WineryRuntime(URL zipFile, List<String> args) throws IOException {
-    this.args = args;
-
-    final var linkerFactory = new DynamicLinkerFactory();
-    linkerFactory.setPrioritizedLinker(new BeansLinker());
-    this.linker = linkerFactory.createLinker();
-
-    try {
-      deployment = Files.createTempDirectory("marid");
-      thread = new Thread(() -> {
-        try {
-          while (!Thread.currentThread().isInterrupted()) {
-            final var task = queue.poll();
-            if (task == null) {
-              Thread.onSpinWait();
-              continue;
-            }
-            switch (task) {
-              case START:
-                try {
-                  run();
-                } catch (Throwable e) {
-                  startError = e;
-                }
-                break;
-              case STOP:
-                try {
-                  destroy();
-                } catch (Throwable e) {
-                  destroyError = e;
-                }
-                break;
-            }
-          }
-        } finally {
-          destroy();
-        }
-      }, toString());
-      cleaner.register(this, thread::interrupt);
-
-      try (final var is = new ZipInputStream(zipFile.openStream(), UTF_8)) {
-        unpack(is);
-      }
-
-      classes = deployment.resolve("classes");
-      resources = deployment.resolve("resources");
-      deps = deployment.resolve("deps");
-      winery = deployment.resolve("winery.xml");
-
-      validate();
-      initialize();
-
-      classLoader = classLoader();
-    } catch (Throwable e) {
+  private WineryRuntime(WineryParams params) {
+    this.classLoader = params.classLoader;
+    this.destroyAction = params.destroyAction;
+    final var winery = params.winery;
+    this.thread = new Thread(() -> {
       try {
+        while (!Thread.currentThread().isInterrupted()) {
+          final var task = queue.poll();
+          if (task == null) {
+            Thread.onSpinWait();
+            continue;
+          }
+          switch (task) {
+            case START:
+              try {
+                run(winery);
+              } catch (Throwable e) {
+                startError = e;
+              }
+              break;
+            case STOP:
+              try {
+                destroy();
+              } catch (Throwable e) {
+                destroyError = e;
+              }
+              break;
+          }
+        }
+      } finally {
         destroy();
-      } catch (Throwable x) {
-        e.addSuppressed(x);
       }
-      throw e;
-    }
+    }, params.name);
   }
 
-  private void unpack(ZipInputStream zipInputStream) throws IOException {
+  public WineryRuntime(URL zipFile, List<String> args) {
+    this(new WineryParams(zipFile, args));
+  }
+
+  @TestOnly
+  public WineryRuntime(String name, ClassLoader classLoader, Winery winery, AutoCloseable destroyAction) {
+    this(new WineryParams(name, new URLClassLoader(new URL[0], classLoader), winery, destroyAction));
+  }
+
+  private static void unpack(Path deployment, ZipInputStream zipInputStream) throws IOException {
     for (var e = zipInputStream.getNextEntry(); e != null; e = zipInputStream.getNextEntry()) {
       try {
         final var target = deployment.resolve(e.getName());
@@ -156,7 +130,7 @@ public final class WineryRuntime implements AutoCloseable {
     }
   }
 
-  private void validate() throws IOException {
+  private static void validate(Path resources, Path deps, Path classes) throws IOException {
     Files.createDirectories(resources);
     Files.createDirectories(deps);
     if (!Files.isDirectory(classes)) {
@@ -164,7 +138,7 @@ public final class WineryRuntime implements AutoCloseable {
     }
   }
 
-  private void initialize() throws IOException {
+  private static void initialize(Path deployment, List<String> args) throws IOException {
     final var propsFile = deployment.resolve("system.properties");
     if (Files.isRegularFile(propsFile)) {
       final var props = new Properties();
@@ -173,22 +147,18 @@ public final class WineryRuntime implements AutoCloseable {
       }
       props.forEach(System.getProperties()::putIfAbsent);
     }
-  }
-
-  private URLClassLoader classLoader() throws IOException {
-    final var urls = new ArrayList<URL>();
-    urls.add(classes.toUri().toURL());
-    urls.add(resources.toUri().toURL());
-    try (final var dirStream = Files.newDirectoryStream(deps, "*.jar")) {
-      for (final var path : dirStream) {
-        urls.add(path.toUri().toURL());
+    for (final var arg: args) {
+      if (arg.startsWith("--") && arg.contains("=")) {
+        final var eqi = arg.indexOf('=');
+        final var key = arg.substring(2, eqi);
+        final var val = arg.substring(eqi + 1).trim();
+        System.getProperties().putIfAbsent(key, val);
       }
     }
-    return new URLClassLoader(urls.toArray(URL[]::new), Thread.currentThread().getContextClassLoader());
   }
 
   public String getId() {
-    return deployment.getFileName().toString();
+    return thread.getName();
   }
 
   public void start() {
@@ -225,7 +195,7 @@ public final class WineryRuntime implements AutoCloseable {
     }
   }
 
-  private void run() {
+  private void run(Winery winery) {
     if (state == State.STARTING || state == State.RUNNING) {
       return;
     }
@@ -234,7 +204,6 @@ public final class WineryRuntime implements AutoCloseable {
     Thread.currentThread().setContextClassLoader(classLoader);
 
     try {
-      final var winery = Xmls.read(this.winery, Winery::new);
       for (final var cellar : winery.getCellars()) {
         cellars.put(cellar.getName(), new CellarRuntime(this, cellar));
       }
@@ -284,7 +253,7 @@ public final class WineryRuntime implements AutoCloseable {
     }
 
     try {
-      MaridFiles.deleteRecursively(deployment);
+      destroyAction.close();
     } catch (Throwable e) {
       exception.addSuppressed(e);
     }
@@ -317,9 +286,70 @@ public final class WineryRuntime implements AutoCloseable {
 
   @Override
   public String toString() {
-    return deployment.getFileName().toString();
+    return getId();
   }
 
   public enum State {NEW, STARTING, RUNNING, TERMINATING, TERMINATED}
   public enum Command {START, STOP}
+
+  private static class WineryParams {
+
+    private final String name;
+    private final URLClassLoader classLoader;
+    private final Winery winery;
+    private AutoCloseable destroyAction;
+
+    private WineryParams(URL zipFile, List<String> args) {
+      final Path deployment;
+      try {
+        deployment = Files.createTempDirectory("marid");
+        this.name = deployment.getFileName().toString();
+        this.destroyAction = () -> MaridFiles.deleteRecursively(deployment);
+
+        try (final var is = new ZipInputStream(zipFile.openStream(), UTF_8)) {
+          unpack(deployment, is);
+        }
+
+        final var classes = deployment.resolve("classes");
+        final var resources = deployment.resolve("resources");
+        final var deps = deployment.resolve("deps");
+        final var winery = deployment.resolve("winery.xml");
+
+        this.winery = Xmls.read(winery, Winery::new);
+
+        validate(resources, deps, classes);
+        initialize(deployment, args);
+
+        classLoader = classLoader(classes, resources, deps);
+      } catch (Throwable e) {
+        if (destroyAction != null) {
+          try {
+            destroyAction.close();
+          } catch (Throwable x) {
+            e.addSuppressed(x);
+          }
+        }
+        throw new IllegalStateException(e);
+      }
+    }
+
+    private WineryParams(String name, URLClassLoader classLoader, Winery winery, AutoCloseable destroyAction) {
+      this.name = name;
+      this.classLoader = classLoader;
+      this.winery = winery;
+      this.destroyAction = destroyAction;
+    }
+
+    private URLClassLoader classLoader(Path classes, Path resources, Path deps) throws IOException {
+      final var urls = new ArrayList<URL>();
+      urls.add(classes.toUri().toURL());
+      urls.add(resources.toUri().toURL());
+      try (final var dirStream = Files.newDirectoryStream(deps, "*.jar")) {
+        for (final var path : dirStream) {
+          urls.add(path.toUri().toURL());
+        }
+      }
+      return new URLClassLoader(urls.toArray(URL[]::new), Thread.currentThread().getContextClassLoader());
+    }
+  }
 }
