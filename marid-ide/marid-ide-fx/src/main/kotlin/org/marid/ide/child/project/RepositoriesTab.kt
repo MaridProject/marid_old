3fdc08f4dcfc8b115151bf6141f7fb011733dfb8
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2020-01-09 22:16:29 +0300

    Close

--- marid-ide/marid-ide-fx/src/main/kotlin/org/marid/ide/child/project/RepositoriesTab.kt
+++ marid-ide/marid-ide-fx/src/main/kotlin/org/marid/ide/child/project/RepositoriesTab.kt
@@ -1,8 +1,11 @@
 package org.marid.ide.child.project
 
 import javafx.scene.control.Tab
+import javafx.scene.control.TableView
+import javafx.scene.control.ToolBar
 import javafx.scene.layout.BorderPane
 import org.marid.fx.i18n.localized
+import org.marid.ide.project.xml.XmlRepository
 import org.springframework.stereotype.Component
 
 @Component
@@ -16,4 +19,11 @@ class RepositoriesTab(contents: RepositoriesTabContents) : Tab(null, contents) {
 @Component
 class RepositoriesTabContents : BorderPane() {
 
+  private val toolbar = ToolBar()
+  private val list = TableView<XmlRepository>()
+
+  init {
+    top = toolbar
+    center = list
+  }
 }
