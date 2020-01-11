64c326d7ebd1a6a1fd1e9457e112e7accdd07fb6
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2020-01-11 16:05:59 +0300

    Close

--- marid-ide/marid-ide-fx/src/main/kotlin/org/marid/ide/child/project/RepositoriesTab.kt
+++ marid-ide/marid-ide-fx/src/main/kotlin/org/marid/ide/child/project/RepositoriesTab.kt
@@ -39,8 +39,19 @@ class RepositoriesTabContents(projectFactory: ObjectFactory<Project>) : BorderPa
         minWidth = 128.0; prefWidth = 160.0; maxWidth = 200.0
         textProperty().bind("Name".localized)
         cellValueFactory = Callback { it.value.name }
+      },
+      TableColumn<XmlRepository, XmlRepository.Type>().apply {
+        minWidth = 80.0; prefWidth = 80.0; maxWidth = 128.0
+        textProperty().bind("Type".localized)
+        cellValueFactory = Callback { it.value.type }
+        style = "-fx-alignment: CENTER;"
+      },
+      TableColumn<XmlRepository, String>("URL").apply {
+        minWidth = 200.0; prefWidth = 250.0; maxWidth = 1200.0
+        cellValueFactory = Callback { it.value.url }
       }
     )
+    columnResizePolicy = TableView.CONSTRAINED_RESIZE_POLICY
   }
 
   init {
