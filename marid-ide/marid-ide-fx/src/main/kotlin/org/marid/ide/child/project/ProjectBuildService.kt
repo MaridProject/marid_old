921471e4ca71897c21bebaf9bfe808f5d6d68052
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2020-01-26 10:42:19 +0300

    Winery

--- marid-ide/marid-ide-fx/src/main/kotlin/org/marid/ide/child/project/ProjectBuildService.kt
+++ marid-ide/marid-ide-fx/src/main/kotlin/org/marid/ide/child/project/ProjectBuildService.kt
@@ -1,18 +1,36 @@
 package org.marid.ide.child.project
 
+import javafx.application.Platform
+import javafx.beans.WeakInvalidationListener
 import javafx.concurrent.Service
 import javafx.concurrent.Task
+import javafx.concurrent.Worker.State.*
+import org.marid.fx.extensions.inf
+import org.marid.fx.extensions.logger
+import org.marid.ide.extensions.bean
 import org.marid.ide.main.IdeServices
+import org.marid.ide.project.Project
+import org.springframework.beans.factory.ObjectFactory
 import org.springframework.stereotype.Component
 import javax.annotation.PostConstruct
 import javax.annotation.PreDestroy
 
 @Component
-class ProjectBuildService(private val services: IdeServices) : Service<Unit>() {
+class ProjectBuildService(
+  private val services: IdeServices,
+  projectFactory: ObjectFactory<Project>
+) : Service<Unit>() {
+
+  private val project = projectFactory.bean
+  private val projectInvalidationListener = WeakInvalidationListener { invalidated() }
+  private var dirty = false
 
   override fun createTask(): Task<Unit> {
     return object : Task<Unit>() {
       override fun call() {
+        project.logger.info("Build started")
+        Thread.sleep(10_000L)
+        project.logger.info("Build finished")
       }
     }
   }
@@ -20,10 +38,28 @@ class ProjectBuildService(private val services: IdeServices) : Service<Unit>() {
   @PostConstruct
   fun onInit() {
     services.add(this)
+    project.observables.forEach { it.addListener(projectInvalidationListener) }
+    stateProperty().addListener { _, _, s ->
+      if (s == SUCCEEDED || s == FAILED || s == CANCELLED) {
+        if (dirty) {
+          dirty = false
+          Platform.runLater { restart() }
+        }
+      }
+    }
   }
 
   @PreDestroy
   fun onDestroy() {
     services.remove(this)
+    project.observables.forEach { it.removeListener(projectInvalidationListener) }
+  }
+
+  private fun invalidated() {
+    if (isRunning) {
+      dirty = true
+    } else {
+      Platform.runLater { restart() }
+    }
   }
 }
