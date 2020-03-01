b99142fb56ffcd933cb02fd2c64b4d1b90d51af0
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2020-03-01 12:13:31 +0300

    Model

--- marid-ide/marid-fx/src/test/kotlin/org/marid/fx/CollectionTest.kt
+++ marid-ide/marid-fx/src/test/kotlin/org/marid/fx/CollectionTest.kt
@@ -4,20 +4,21 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import java.util.*
+import java.util.concurrent.atomic.AtomicBoolean
 
 @Tag("broken")
 class CollectionTest {
 
   @Test
   fun sort() {
-    var changed = false
+    val changed = AtomicBoolean()
     val list = object : ArrayList<String>() {
       override fun sort(c: Comparator<in String>?) {
-        changed = true
+        changed.set(true)
         super.sort(c)
       }
     }
     list.sortWith(compareBy { it })
-    assertTrue(changed)
+    assertTrue(changed.get())
   }
 }
