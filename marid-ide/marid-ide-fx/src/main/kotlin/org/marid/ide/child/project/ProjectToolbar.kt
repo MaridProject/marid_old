2b6ce85cf2ba9a33bd6749095908b2c73d1e302c
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2020-03-08 21:46:24 +0300

    Model

--- marid-ide/marid-ide-fx/src/main/kotlin/org/marid/ide/child/project/ProjectToolbar.kt
+++ marid-ide/marid-ide-fx/src/main/kotlin/org/marid/ide/child/project/ProjectToolbar.kt
@@ -1,9 +1,14 @@
 package org.marid.ide.child.project
 
+import com.google.common.io.MoreFiles
+import javafx.scene.control.Separator
 import javafx.scene.control.ToolBar
 import org.marid.fx.action.Fx
 import org.marid.fx.action.toolButton
+import org.marid.ide.extensions.bean
+import org.marid.ide.project.Project
 import org.marid.spring.init.Init
+import org.springframework.beans.factory.ObjectFactory
 import org.springframework.stereotype.Component
 
 @Component
@@ -15,6 +20,18 @@ class ProjectToolbar : ToolBar() {
       text = "Build",
       icon = "icons/build.png",
       h = { buildService.restart() },
+      disabled = buildService.runningProperty()
+    ).toolButton
+  }
+
+  @Suppress("UnstableApiUsage")
+  @Init
+  fun initClearCache(project: ObjectFactory<Project>, buildService: ProjectBuildService) {
+    items += Separator()
+    items += Fx(
+      text = "Clear cache",
+      icon = "icons/clean.png",
+      h = { MoreFiles.deleteDirectoryContents(project.bean.cacheDepsDirectory) },
       disabled = buildService.runningProperty()
     ).toolButton
   }
