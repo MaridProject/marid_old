39fae0aab59f15de66d404c48f840bca6b7a0586
Dmitry Ovchinnikov <d.ovchinnikow@gmail.com>
2020-03-14 10:10:52 +0300

    Model

--- marid-ide/marid-fx/src/test/kotlin/org/marid/fx/CollectionTest.kt
+++ marid-ide/marid-fx/src/test/kotlin/org/marid/fx/CollectionTest.kt
@@ -2,29 +2,12 @@ package org.marid.fx
 
 import javafx.collections.FXCollections
 import javafx.collections.ListChangeListener
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
-import java.util.*
-import java.util.concurrent.atomic.AtomicBoolean
-import kotlin.collections.ArrayList
 import kotlin.test.assertEquals
 
 @Tag("normal")
 class CollectionTest {
-
-  @Test
-  fun sort() {
-    val changed = AtomicBoolean()
-    val list = object : ArrayList<String>(listOf("1", "7", "2", "10", "3")) {
-      override fun sort(c: Comparator<in String>?) {
-        changed.set(true)
-        super.sort(c)
-      }
-    }
-    list.sortWith(compareBy { it })
-    assertTrue(changed.get())
-  }
 
   @Test
   fun sortSubList() {
